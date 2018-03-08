ebb15d24da1daa5cc8f652c545bb0669a45df0bd
Aceeri <conmcclusk@gmail.com>
2018-03-08 19:28:34 +0000

    Adjust deadline to 30 seconds

--- mapi.go
+++ mapi.go
@@ -338,7 +338,7 @@ func (c *MapiConn) getBytes(count int) ([]byte, error) {
 	for read < count {
 		b := make([]byte, count-read)
 
-		c.conn.SetDeadline(time.Now().Add(10 * time.Second))
+		c.conn.SetDeadline(time.Now().Add(30 * time.Second))
 		n, err := c.conn.Read(b)
 		if err != nil {
 			return nil, err
