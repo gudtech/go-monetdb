3ccb1c48725382d7544f5a7a318cb6676271c334
Aceeri <conmcclusk@gmail.com>
2018-12-17 18:00:48 -0800

    CopyInto support

--- conn.go
+++ conn.go
@@ -7,6 +7,7 @@ package monetdb
 import (
 	"database/sql/driver"
 	"fmt"
+	"strings"
 )
 
 type Conn struct {
@@ -62,4 +63,50 @@ func (c *Conn) cmd(cmd string) (string, error) {
 func (c *Conn) execute(q string) (string, error) {
 	cmd := fmt.Sprintf("s%s;", q)
 	return c.cmd(cmd)
+}
+
+func (c *Conn) CopyInto(tableName string, channel chan []interface{}, rowCount int64) error {
+	if rowCount == 0 {
+		return fmt.Errorf("no rows")
+	}
+
+	if c.mapi.State != MAPI_STATE_READY {
+		return fmt.Errorf("Database not connected")
+	}
+
+	//var monetNull string = "mtwFTyme5SWmzgokt8Npopvr26XyaX7"
+	var monetNull string = "NULL"
+	query := fmt.Sprintf("COPY %d RECORDS INTO %s FROM STDIN USING DELIMITERS ',','\\n','\"' NULL AS '%s' LOCKED", rowCount, tableName, monetNull)
+
+	if err := c.mapi.putBlock([]byte(query)); err != nil {
+		return err
+	}
+
+	for {
+		row, more := <-channel
+		if !more {
+			break
+		}
+
+		var convertedValues []string
+		for _, field := range row {
+			converted, err := ConvertToMonet(field)
+			if err != nil {
+				return fmt.Errorf("conversion: %s", err)
+			}
+
+			convertedValues = append(convertedValues, converted)
+		}
+
+		block := fmt.Sprintf("%s\n", strings.Join(convertedValues, ","))
+		if err := c.mapi.putBlock([]byte(block)); err != nil {
+			return err
+		}
+	}
+
+	if err := c.mapi.putBlock([]byte("\x0D")); err != nil {
+		return err
+	}
+
+	return nil
 }
